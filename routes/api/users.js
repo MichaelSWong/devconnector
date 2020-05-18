9c8081ceae6fe682226525bbcc6c3751b2ccdc29
MICHAELSWONG <mizik_jah@yahoo.com>
2020-05-18 14:33:11 -0400

    Login routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
-
+const config = require('config');
 const User = require('../../models/User');
 
 // @route   POST api/users
@@ -55,11 +56,24 @@ router.post(
 
       await user.save();
 
-      // Return jsonwebtoken
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server error');
+      res.status(500).send('Server Error');
     }
   }
 );
